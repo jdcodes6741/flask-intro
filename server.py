4328d379811f70521c5873307c1fe37c1318ee2e
Your Name <you@example.com>
2020-02-11 13:53:18 -0800

    completed flask-intro

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return "<!doctype html><html>Hi! This is the <a href='/hello'>home page.</a></html>"
 
 
 @app.route("/hello")
@@ -33,24 +33,42 @@ def say_hello():
       </head>
       <body>
         <h1>Hi There!</h1>
-        <form action="/greet">
-          What's your name? <input type="text" name="person">
-          <input type="submit" value="Submit">
-        </form>
+        <div>
+          <form action="/greet" method="POST">
+            What's your name? <input type="text" name="person">
+            <input type="submit" value="Submit">
+
+            <select name="name">
+              <option value="awesome">Awesome</option>
+              <option value="ducky">Ducky</option>
+              <option value="wowza">Wowza</option>
+            </select>
+          </form>
+        </div>
+        <div>
+           <form action="/diss" method="POST">
+            What's your name? <input type="text" name="person">
+            <input type="submit" value="Submit">
+
+            <select name="choose-diss">
+              <option value="ugly">Ugly</option>
+              <option value="smelly">Smelly</option>
+              <option value="bitter">Bitter</option>
+            </select>
+          </form>
+        </div>
       </body>
     </html>
     """
 
 
-@app.route("/greet")
+@app.route("/greet", methods=['POST'])
 def greet_person():
     """Get user by name."""
 
-    player = request.args.get("person")
+    player = request.form.get("person")
 
-    compliment = choice(AWESOMENESS)
-
-    y = x
+    compliment = request.form.get("name")
 
     return """
     <!doctype html>
@@ -65,7 +83,28 @@ def greet_person():
     """.format(player, compliment)
 
 
+@app.route("/diss", methods=['POST'])
+def diss_person():
+    """Get user by name."""
+
+    player = request.form.get("person")
+
+    diss = request.form.get("choose-diss")
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Diss</title>
+      </head>
+      <body>
+        Hi, {}! I think you're {}!
+      </body>
+    </html>
+    """.format(player, diss)
+
+
 if __name__ == "__main__":
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
